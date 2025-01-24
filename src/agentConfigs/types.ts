dcc7ff5272772e02d0b0ea9e2081debf80890370
Sarath@MbP <shekkizh@usc.edu>
2025-01-24 15:30:31 -0800

    removing files and updating to audentic

--- src/agentConfigs/types.ts
+++ src/agentConfigs/types.ts
@@ -1,11 +1,11 @@
-import { AgentConfig, Tool } from "@voiceact/mabel/types";
+import { AgentConfig, Tool } from "@audentic/react";
 
 export interface CreateAgentInput extends Omit<AgentConfig, "id" | "userId"> {
-    name: string;
-    instructions: string;
-    firstMessage?: string;
-    language?: string;
-    tools: Tool[];
-    toolLogic?: AgentConfig["toolLogic"];
-    downstreamAgents?: AgentConfig["downstreamAgents"];
+  name: string;
+  instructions: string;
+  firstMessage?: string;
+  language?: string;
+  tools: Tool[];
+  toolLogic?: AgentConfig["toolLogic"];
+  downstreamAgents?: AgentConfig["downstreamAgents"];
 }
