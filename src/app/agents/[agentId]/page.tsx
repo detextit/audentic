479746b48ce4e50f415f3a68a366beee965cc8d9
Pooja Singh <singhpoo@usc.edu>
2025-01-26 11:36:59 -0800

    fixing routing

---
+++ src/app/agents/[agentId]/page.tsx
@@ -0,0 +1,6 @@
+import Home from '@/app/Home';
+
+// This is just a wrapper to enable dynamic routing while keeping the Home component as the main UI
+export default function AgentPage() {
+    return <Home />;
+} 
