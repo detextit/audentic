db9a4cc59e09f0c0cfedd5320a739c9578d96623
Pooja Singh <singhpoo@usc.edu>
2025-01-28 21:59:51 -0800

    langing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import LandingPage from "./landing/page";
 import Home from "./Home";
 
-export default function Page() {
-  return <Home />;
+export default async function Page() {
+  const { userId } = await auth();
+
+  // If user is signed in, show home dashboard, otherwise show landing page
+  return userId ? <Home /> : <LandingPage />;
 }
